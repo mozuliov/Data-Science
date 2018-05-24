d90a9983c2334ee7f59a50b8bd900f5c7dbdeebd
Andrey Mozuliov <36289040+mozuliov@users.noreply.github.com>
2018-05-24 13:10:53 -0700

    Workable script
    Major location and eight locations around are pulling correctly. Data recorded every 3 minutes in the file associated with the calendar day. New file created every day at midnight. 
    Wind gust considered as addition to wind speed.
    Day quadrant feature added: four parts of the day starting at sunrise, second quadrant ends with sunset, day and night midpoints calculated as halfway between sunset and sunrise.

--- weather_openweathermap_m.py
+++ weather_openweathermap_m.py
@@ -15,134 +15,146 @@ while True:
     # 6122085 Richmond, BC
     # 6173331 Vancouver, BC
     response = requests.get('http://api.openweathermap.org/data/2.5/group?id=6159905,5911606,6111706,6065686,6049429,6180961,5937615,6122085,6173331&units=metric&APPID=cff8ba42de36c013f53acaa95cee63fc')
-
-
-    print (response.content)
     data = response.json()
     print (data)
-    # data1 = data[1]
-    # print (data1)
 
+    record = []     # This list will contain data for the new line to write to csv file
+    wind_dir = ['NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA','NA']   # In case wind direction not available we will be using most recent available data
 
-    datestamp = datetime.datetime.now()
+    record.append(datetime.datetime.now())  # date and time key of the record
 
-    i = 0
-
-
-    # Temperature in C
-    try:
-        data['list'][i]['main']['temp']
-    except KeyError:
-        Temp = 'NA'
+    # Write down the part of the day as one of the feature:
+    #  1 for sunrise to midpoint between sunrise and sunset interval
+    #  2 for interval between midpoint and sunset
+    #  3 for interval between sunset and midpoint between sunset and sunrise (same day for simplification)
+    #  4 for interval between midpoint between sunset and sunrise and sunrise
+    if (datetime.datetime.utcnow().timestamp() - data['list'][0]['sys']['sunset']) > 0:        # after sunset
+        day_quadrant = 3
     else:
-        Temp = data['list'][i]['main']['temp']
+        if (datetime.datetime.utcnow().timestamp() - data['list'][0]['sys']['sunrise']) > 0:   # day
+            if (datetime.datetime.utcnow().timestamp() - (data['list'][0]['sys']['sunset'] - data['list'][0]['sys']['sunrise']) / 2 + data['list'][0]['sys']['sunrise']) > 0:
+                day_quadrant = 2
+            else:
+                day_quadrant = 1
+        else:                                                                   # before sunrise
+            if (datetime.datetime.utcnow().timestamp() - (data['list'][0]['sys']['sunrise'] - data['list'][0]['sys']['sunset'] - 86400) / 2 + data['list'][0]['sys']['sunset'] + 86400) > 0:
+                day_quadrant = 4
+            else:
+                day_quadrant = 3
 
-    print(Temp)
+    record.append(day_quadrant)
 
-    # # Air pressure
-    # try:
-    #     data["main"]["pressure"]
-    # except KeyError:
-    #     curr_pressure = 'NA'
-    # else:
-    #     curr_pressure = data["main"]["pressure"]
-    #
-    # # Humidity
-    # try:
-    #     data["main"]["humidity"]
-    # except KeyError:
-    #     curr_humidity = 'NA'
-    # else:
-    #     curr_humidity = data["main"]["humidity"]
-    #
-    # # Wind speed m/sec
-    # try:
-    #     data["wind"]["speed"]
-    # except KeyError:
-    #     curr_wind = 'NA'
-    # else:
-    #     curr_wind = data["wind"]["speed"]
-    #
-    # # Wind direction degree
-    # try:
-    #     data["wind"]["deg"]
-    # except KeyError:
-    #     curr_wind_dir = 'NA'
-    # else:
-    #     curr_wind_dir = data["wind"]["deg"]
-    #
-    # # Wind gust
-    # try:
-    #     data["wind"]["gust"]
-    # except KeyError:
-    #     curr_wind_gust = 'NA'
-    # else:
-    #     curr_wind_gust = data["wind"]["gust"]
-    #
-    # # Clouds %
-    # try:
-    #     data["clouds"]["all"]
-    # except KeyError:
-    #     curr_clouds = 'NA'
-    # else:
-    #     curr_clouds = data["clouds"]["all"]
-    #
-    # # Rain volume for last 3 hours
-    # try:
-    #     data["rain"]["3h"]
-    # except KeyError:
-    #     curr_rain3h = 'NA'
-    # else:
-    #     curr_rain3h = data["rain"]["3h"]
-    #
-    # # Snow volume for last 3 hours
-    # try:
-    #     data["snow"]["3h"]
-    # except KeyError:
-    #     curr_snow3h = 'NA'
-    # else:
-    #     curr_snow3h = data["snow"]["3h"]
-    #
-    # # Visibility m
-    # try:
-    #     data["visibility"]
-    # except KeyError:
-    #     curr_visibility = 'NA'
-    # else:
-    #     curr_visibility = data["visibility"]
-    #
-    # # write to csv file
-    # if datetime.datetime.now().hour == 0 and datetime.datetime.now().minute == 0:
-    #     fieldnames = ['Datestamp',
-    #                   'Temperature',
-    #                   'Preasure',
-    #                   'Humidity',
-    #                   'Wind_speed',
-    #                   'Wind_dir',
-    #                   'Wind_gust',
-    #                   'Clouds',
-    #                   'Rain',
-    #                   'Snow',
-    #                   'Visibility']
-    #     with open(str(datetime.date.today()) + '.csv', 'a', newline = '') as f:
-    #         writer = csv.writer(f)
-    #         writer.writerow(fieldnames)
-    # fields = [datestamp,
-    #           curr_temperature,
-    #           curr_pressure,
-    #           curr_humidity,
-    #           curr_wind,
-    #           curr_wind_dir,
-    #           curr_wind_gust,
-    #           curr_clouds,
-    #           curr_rain3h,
-    #           curr_snow3h,
-    #           curr_visibility]
-    # with open(str(datetime.date.today()) + '.csv', 'a', newline = '') as f:
-    #     writer = csv.writer(f)
-    #     writer.writerow(fields)
+    for city in range(9):
+        # Temperature in C
+        try:
+            data['list'][city]['main']['temp']
+        except KeyError:
+            record.append('NA')
+        else:
+            record.append(data['list'][city]['main']['temp'])
 
+        # Air pressure
+        try:
+            data['list'][city]['main']['pressure']
+        except KeyError:
+            record.append('NA')
+        else:
+            record.append(data['list'][city]['main']['pressure'])
 
+        # Humidity
+        try:
+            data['list'][city]['main']['humidity']
+        except KeyError:
+            record.append('NA')
+        else:
+            record.append(data['list'][city]['main']['humidity'])
 
-    time.sleep(60)
+        # Wind speed m/sec
+        try:
+            data['list'][city]['wind']['speed']
+        except KeyError:
+            record.append('NA')
+        else:
+            record.append(data['list'][city]['wind']['speed'])
+
+        # Wind direction degree
+        try:
+            data['list'][city]['wind']['deg']
+        except KeyError:
+            record.append(wind_dir[city])
+        else:
+            record.append(data['list'][city]['wind']['deg'])
+            wind_dir[city] = data['list'][city]['wind']['deg']
+
+        # Wind gust considered here as an addition m/sec to wind speed
+        try:
+            data['list'][city]['wind']['gust']
+        except KeyError:
+            record.append(0)
+        else:
+            record.append(data['list'][city]['wind']['gust']-data['list'][city]['wind']['speed'])
+
+        # Clouds %
+        try:
+            data['list'][city]['clouds']['all']
+        except KeyError:
+            record.append('NA')
+        else:
+            record.append(data['list'][city]['clouds']['all'])
+
+        # Rain volume for last 3 hours
+        try:
+            data['list'][city]['rain']['3h']
+        except KeyError:
+            record.append('NA')
+        else:
+            record.append(data['list'][city]['rain']['3h'])
+
+        # Snow volume for last 3 hours
+        try:
+            data['list'][city]['snow']['3h']
+        except KeyError:
+            record.append('NA')
+        else:
+             record.append(data['list'][city]['snow']['3h'])
+
+        # Visibility m
+        try:
+            data['list'][city]['visibility']
+        except KeyError:
+            record.append('NA')
+        else:
+            record.append(data['list'][city]['visibility'])
+
+    try:
+        with open(str(datetime.date.today()) + '.csv', 'r') as f:
+            with open(str(datetime.date.today()) + '.csv', 'a', newline = '') as f:
+                writer = csv.writer(f)
+                writer.writerow(record)
+    except IOError as x:
+        fieldnames = ['Datestamp', 'Day_Quadrant',
+                      '0_Temp', '0_Pressure', '0_Humidity', '0_Wind_Speed', '0_Wind_Dir', '0_Wind_Gust', '0_Clouds',
+                      '0_Rain', '0_Snow', '0_Visibility',
+                      '1_Temp', '1_Pressure', '1_Humidity', '1_Wind_Speed', '1_Wind_Dir', '1_Wind_Gust', '1_Clouds',
+                      '1_Rain', '1_Snow', '1_Visibility',
+                      '2_Temp', '2_Pressure', '2_Humidity', '2_Wind_Speed', '2_Wind_Dir', '2_Wind_Gust', '2_Clouds',
+                      '2_Rain', '2_Snow', '2_Visibility',
+                      '3_Temp', '3_Pressure', '3_Humidity', '3_Wind_Speed', '3_Wind_Dir', '3_Wind_Gust', '3_Clouds',
+                      '3_Rain', '3_Snow', '3_Visibility',
+                      '4_Temp', '4_Pressure', '4_Humidity', '4_Wind_Speed', '4_Wind_Dir', '4_Wind_Gust', '4_Clouds',
+                      '4_Rain', '4_Snow', '4_Visibility',
+                      '5_Temp', '5_Pressure', '5_Humidity', '5_Wind_Speed', '5_Wind_Dir', '5_Wind_Gust', '5_Clouds',
+                      '5_Rain', '5_Snow', '5_Visibility',
+                      '6_Temp', '6_Pressure', '6_Humidity', '6_Wind_Speed', '6_Wind_Dir', '6_Wind_Gust', '6_Clouds',
+                      '6_Rain', '6_Snow', '6_Visibility',
+                      '7_Temp', '7_Pressure', '7_Humidity', '7_Wind_Speed', '7_Wind_Dir', '7_Wind_Gust', '7_Clouds',
+                      '7_Rain', '7_Snow', '7_Visibility',
+                      '8_Temp', '8_Pressure', '8_Humidity', '8_Wind_Speed', '8_Wind_Dir', '8_Wind_Gust', '8_Clouds',
+                      '8_Rain', '8_Snow', '8_Visibility']
+
+        with open(str(datetime.date.today()) + '.csv', 'a', newline = '') as f:
+            writer = csv.writer(f)
+            writer.writerow(fieldnames)
+
+    time.sleep(180)
 
